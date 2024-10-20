e14053717ff52a086c8b189e60b691f0e6c21aab
Moroz-Evgeny <ya.moroz2020@gmail.com>
2024-10-20 17:46:35 +0300

    'aye'

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.secret_key = 'ofjoiejj4983f4398j9f092k0k0ef0ejf093'
 
-lskdjfoisdjfoisdjoiffs
 
 # Модель пользователя
 class User(db.Model):
